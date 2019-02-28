d97cedfd97e0d7c1a4dd30f846333c0409b59ee5
Mike Sheffer <mike.sheffer@jci.com>
2019-02-28 17:44:26 -0500

    Part 3, Commit 5

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Http } from '@angular/http';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +9,7 @@ import { Http } from '@angular/http';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) {
   }
