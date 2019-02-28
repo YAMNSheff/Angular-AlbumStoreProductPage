a651a96641afea82b247a48e3f576d66558f93c3
Mike Sheffer <mike.sheffer@jci.com>
2019-02-28 16:47:31 -0500

    Part 3, Commit 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
@@ -8,6 +10,10 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-
+    return this._http.get(this._albumUrl).map(
+      response => {
+        response.json();
+      }
+    )
   }
 }
